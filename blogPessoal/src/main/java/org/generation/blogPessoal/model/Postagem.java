fd47c32662a4abc1f8c3857a5295e82ddbf04ea5
Danielly Nunes <danym.nunes@gmail.com>
2022-08-02 16:13:45 -0300

    ATIVIDADE CRUD BACKEND BLOG-PESSOAL – PARTE 03

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -15,47 +16,69 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity 
-@Table(name = "postagem")
+@Table(name = "postagem") 
 public class Postagem {
-	
+
 	@Id 
-	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
 	private Long id;
-	
+
 	@NotNull 
-	@Size(min = 5, max = 100)
+	@Size(min = 5, max = 100) 
 	private String titulo;
-	
+
 	@NotNull
 	@Size(min = 10, max = 500)
 	private String texto;
-	
+
 	@UpdateTimestamp 
 	private LocalDateTime data;
-	
+
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getTexto() {
 		return texto;
 	}
+
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
+
 	public LocalDateTime getData() {
 		return data;
 	}
+
 	public void setData(LocalDateTime data) {
 		this.data = data;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 }
